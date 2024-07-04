d802d6a23d7fc2c4072527f9d393edc28dfd69dd
Jeffrey L <158185183+Jeffreasy@users.noreply.github.com>
2024-07-04 03:50:38 +0200

    Add initial project setup with Express and MongoDB

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
